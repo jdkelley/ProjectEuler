a338dcf93db0836d992b851d3939fb6af4a6404e
Joshua Kelley <jdkelley@samaritan.org>
2016-04-14 14:29:48 -0400

    Project Euler 005
    - Add base project with prompt.

---
+++ Euler-005-SmallestMultiple/Euler-005-SmallestMultiple/main.swift
@@ -0,0 +1,28 @@
+//
+//  main.swift
+//  Euler-005-SmallestMultiple
+//
+//  Created by Joshua Kelley on 4/14/16.
+//  Copyright © 2016 Joshua Kelley. All rights reserved.
+//
+
+/**
+ 
+ Smallest multiple
+ Project Euler : Problem 5
+ 
+ * --------------------------------------------------
+ Prompt:
+ 
+ 2520 is the smallest number that can be divided 
+ by each of the numbers from 1 to 10 without 
+ any remainder.
+ 
+ What is the smallest positive number that is evenly 
+ divisible by all of the numbers from 1 to 20?
+ 
+ * --------------------------------------------------
+ 
+ Answer: ??
+ 
+ */
